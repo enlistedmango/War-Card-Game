c19bc106f8016b631577acc685f1a15fdfa1ef5c
enlistedmango <jamiemccallum0@gmail.com>
2023-05-29 17:34:52 +0100

    Initial Commit

---
+++ War Card Game/ContentView.swift
@@ -0,0 +1,89 @@
+//
+//  ContentView.swift
+//  War Card Game
+//
+//  Created by Jamie McCallum on 24/05/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var playerCard = "card7"
+    @State var cpuCard = "card13"
+    
+    @State var playerScore = 0
+    @State var cpuScore = 0
+    
+    var body: some View {
+        
+        ZStack{
+            Image("background-cloth")
+                .resizable()
+                .ignoresSafeArea()
+
+            VStack{
+                Spacer()
+                Image("logo")
+                Spacer()
+                HStack{
+                    Spacer()
+                    Image(playerCard)
+                    Spacer()
+                    Image(cpuCard)
+                    Spacer()
+                }
+                Spacer()
+                
+                Button {
+                    deal()
+                } label: {
+                    Image("button")
+                }
+
+                
+                Spacer()
+                HStack{
+                    Spacer()
+                    VStack{
+                        Text("Player").font(.headline).padding(.bottom, 10.0)
+                        Text(String(playerScore)).font(.largeTitle)
+                    }
+                    Spacer()
+                    VStack{
+                        Text("CPU").font(.headline).padding(.bottom, 10.0)
+                        Text(String(cpuScore)).font(.largeTitle)
+                    }
+                    Spacer()
+                }.foregroundColor(.white)
+                Spacer()
+                
+                
+            }
+        }
+    }
+    
+    func deal() {
+        // Randomise the players card
+        @State var playerCardValue = Int.random(in: 2...14)
+        playerCard = "card" + String(playerCardValue)
+        
+        // Randomise the cpus card
+        @State var cpuCardValue = Int.random(in: 2...14)
+        cpuCard = "card" + String(cpuCardValue)
+        
+        // Update the scores
+        if playerCardValue > cpuCardValue {
+            playerScore += 1
+        } else if playerCardValue < cpuCardValue {
+            cpuScore += 1
+        }
+    }
+    
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
